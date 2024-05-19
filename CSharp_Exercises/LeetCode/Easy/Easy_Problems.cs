f3506e5531f007fde111e76ae2c1865377aadaff
WilliamPrakash <willprakash@gmail.com>
2024-05-19 14:43:58 -0400

    Working on problem 66.

---
+++ CSharp_Exercises/LeetCode/Easy/Easy_Problems.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+namespace CSharp_Exercises.LeetCode.Easy
+{
+	
+	public class Easy_Problems
+	{
+        /* 66. Plus One
+			Given a large int represented as in int array, increment
+			the large int by 1 and return the resulting array
+			of digits
+		*/
+		public int[] PlusOne(int[] digits)
+		{
+			string numStr = "";
+			// add nums to string
+			for (int i = 0; i < digits.Length ; i++)
+			{
+				numStr += digits[i].ToString();
+			}
+			numStr = (Int16.Parse(numStr) + 1).ToString();
+			for (int i = 0; i < digits.Length; i++)
+			{
+				digits[i] = Int16.Parse(numStr[i].ToString());
+			}
+
+			return digits;
+		}
+    }
+}
+
--- CSharp_Exercises/Program.cs
+++ CSharp_Exercises/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using CSharp_Exercises.LeetCode.Easy;
+
+
+Easy_Problems easyProblems = new Easy_Problems();
+/*
+Input: digits = [1,2,3]
+Output: [1,2,4]
+
+Input: [9]
+Output: [1,0] (currently outputting [1]
+*/
+//int[] arr = {1, 2, 3};
+int[] arr = { 9 };
+easyProblems.PlusOne(arr);
+
+
 
