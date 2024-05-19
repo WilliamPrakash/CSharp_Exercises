91d89753d8965976faa5b9516820d0add6f19e4c
WilliamPrakash <willprakash@gmail.com>
2024-05-19 16:30:25 -0400

    LeetCode problem 20. solved w/help

--- CSharp_Exercises/LeetCode/Easy/Easy_Problems.cs
+++ CSharp_Exercises/LeetCode/Easy/Easy_Problems.cs
@@ -6,24 +6,51 @@ namespace CSharp_Exercises.LeetCode.Easy
 	
 	public class Easy_Problems
 	{
-        /* 66. Plus One
+        /* 20. Valid Parentheses
+			Given a string s containing just the characters
+			'(', ')', '{', '}', '[' and ']', determine if
+			the input string is valid.
+		*/
+        public bool IsValid(string s)
+		{
+			// Short java solution - phoenix12steve
+			Stack<char> brackets = new Stack<char>();
+			for (int i = 0; i < s.Length; i++)
+			{
+				// If opening bracket, push closing bracket onto stack
+				if (s[i] == '(') brackets.Push(')');
+				else if (s[i] == '{') brackets.Push('}');
+                else if (s[i] == '[') brackets.Push (']');
+				// take closing brackets and compare it to 
+                else if (brackets.Count == 0 || brackets.Pop() != s[i]) return false;
+            }
+
+			return brackets.Count == 0;
+		}
+
+
+		/* 66. Plus One
 			Given a large int represented as in int array, increment
 			the large int by 1 and return the resulting array
 			of digits
 		*/
 		public int[] PlusOne(int[] digits)
 		{
-			string numStr = "";
-			// add nums to string
+			// Add each digit to a string
+			/*string numStr = "";
 			for (int i = 0; i < digits.Length ; i++)
 			{
 				numStr += digits[i].ToString();
 			}
-			numStr = (Int16.Parse(numStr) + 1).ToString();
-			for (int i = 0; i < digits.Length; i++)
+			numStr = (Int64.Parse(numStr) + 1).ToString();
+			// Increase digits' length by 1
+			digits = new int[numStr.Length];
+			for (int i = 0; i < numStr.Length; i++)
 			{
-				digits[i] = Int16.Parse(numStr[i].ToString());
-			}
+				var x = Int64.Parse(numStr[i].ToString());
+				digits[i] = (int)Int64.Parse(numStr[i].ToString());
+				Console.WriteLine(digits);
+			}*/
 
 			return digits;
 		}
--- CSharp_Exercises/Program.cs
+++ CSharp_Exercises/Program.cs
@@ -12,8 +12,23 @@ Input: [9]
 Output: [1,0] (currently outputting [1]
 */
 //int[] arr = {1, 2, 3};
-int[] arr = { 9 };
-easyProblems.PlusOne(arr);
+//int[] arr = { 9 };
+//int[] arr = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+//int[] arr = { 7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6};
+
+//easyProblems.PlusOne(arr);
+
+/*
+Input: s = "()"
+Output: true
+
+Input: s = "()[]{}"
+Output: true
+
+Input: s = "(]"
+Output: false 
+*/
+easyProblems.IsValid("()[]{}");
 
 
 
