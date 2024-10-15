220a3d35094e98fd244f52a9a118d65c810fea99
WilliamPrakash <willprakash@gmail.com>
2024-10-15 16:20:31 -0400

    working on a linked list

---
+++ CSharp_Exercises/Data_Structures/LinkedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace CSharp_Exercises.Data_Structures
+{
+	public class LinkedList
+	{
+		public Node head;
+		public LinkedList(int firstNodeData)
+		{
+			head = new Node(firstNodeData);
+		}
+
+		public void add(int data)
+		{
+			Node current = head;
+			while (current.next != null)
+			{
+				current.next = current.next.next;
+			}
+			Console.WriteLine(current);
+			current.next = new Node(data);
+			Console.WriteLine(this);
+
+		}
+
+	}
+
+	public class Node
+	{
+		public int data;
+		public Node next;
+		public Node(int x)
+		{
+			data = x;
+			next = null;
+		}
+	}
+}
+
--- CSharp_Exercises/Program.cs
+++ CSharp_Exercises/Program.cs
@@ -1,40 +1,9 @@
-﻿using System;
-using System.Collections;
-using CSharp_Exercises.LeetCode.Easy;
+﻿using CSharp_Exercises.LeetCode.Easy;
+using CSharp_Exercises.Data_Structures;
 
 
-Easy_Problems easyProblems = new Easy_Problems();
-/*
-Input: digits = [1,2,3]
-Output: [1,2,4]
-
-Input: [9]
-Output: [1,0] (currently outputting [1]
-*/
-//int[] arr = {1, 2, 3};
-//int[] arr = { 9 };
-//int[] arr = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-//int[] arr = { 7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6};
-
-//easyProblems.PlusOne(arr);
-
-/*
-Input: s = "()"
-Output: true
-
-Input: s = "()[]{}"
-Output: true
-
-Input: s = "(]"
-Output: false 
-*/
-//easyProblems.IsValid("()[]{}");
-
-//int[] x = { 2, 7, 11, 15 };
-//easyProblems.TwoSum(x, 9);
-int[] x1 = { 3, 2, 4 };
-easyProblems.TwoSum(x1, 6);
-int[] x2 = { 3, 3 };
-easyProblems.TwoSum(x2, 6);
-
+//Easy_Problems easyProblems = new Easy_Problems();
+LinkedList ll = new LinkedList(5);
+ll.add(4);
+ll.add(3);
 
