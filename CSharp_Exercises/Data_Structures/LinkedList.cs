e430ad779318486e486a1edf88d568cb3473483b
WilliamPrakash <willprakash@gmail.com>
2024-10-23 11:54:32 -0400

    Working (with limited functionality) LinkedList

--- CSharp_Exercises/Data_Structures/LinkedList.cs
+++ CSharp_Exercises/Data_Structures/LinkedList.cs
@@ -4,30 +4,62 @@ namespace CSharp_Exercises.Data_Structures
 	public class LinkedList
 	{
 		public Node head;
+		public int size;
 		public LinkedList(int firstNodeData)
 		{
 			head = new Node(firstNodeData);
+			size++;
 		}
 
-		public void add(int data)
+		public void add(int data) // Adds to end
 		{
 			Node current = head;
 			while (current.next != null)
 			{
-				current.next = current.next.next;
+				current = current.next;
 			}
-			Console.WriteLine(current);
 			current.next = new Node(data);
-			Console.WriteLine(this);
+			size++;
+		}
+
+		public void addAt(int data, int index) // Adds new node to index slot
+		{
 
 		}
+
+		public void remove() // Removes from end
+		{
+			Node current = head;
+            while (current.next.next != null)
+            {
+				current = current.next;
+            }
+			Console.WriteLine(current);
+			current.next = null;
+			size--;
+        }
+
+		public void removeAt(int index) // Remove at index (0 based)
+		{
+			/* this doesn't take into account cases where the index isn't in the linked list */
+			Node current = head;
+			int i = 0;
+			// iterate over linked list, while keeping track of index
+			while (i < index-1) // -1 so we get to the index before the one we wanna remove
+			{
+				current = current.next;
+				i++;
+			}
+			current.next = current.next.next;
+            Console.WriteLine(current);
+        }
 
 	}
 
 	public class Node
 	{
 		public int data;
-		public Node next;
+		public Node? next;
 		public Node(int x)
 		{
 			data = x;
--- CSharp_Exercises/Program.cs
+++ CSharp_Exercises/Program.cs
@@ -6,4 +6,7 @@ using CSharp_Exercises.Data_Structures;
 LinkedList ll = new LinkedList(5);
 ll.add(4);
 ll.add(3);
+//ll.remove();
+ll.add(2);
+ll.removeAt(2); // 0 based index
 
