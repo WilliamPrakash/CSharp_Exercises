45cd4b7db897962b7c7cfd7db6a7a00f8ddc9498
WilliamPrakash <willprakash@gmail.com>
2025-02-04 10:37:58 -0500

    Working on LeetCode problem 9.

--- CSharp_Exercises/LeetCode/Easy/Easy_Problems.cs
+++ CSharp_Exercises/LeetCode/Easy/Easy_Problems.cs
@@ -6,23 +6,15 @@ namespace CSharp_Exercises.LeetCode.Easy
 	
 	public class Easy_Problems
 	{
-        /* 1. Two Sum
+        /* 1. Two Sum ***Solved
 			Given an array of integers nums and an integer target, return indices of the
 			two numbers such that they add up to target.
-
 			You may assume that each input would have exactly one solution,
 			and you may not use the same element twice.
-
-			You can return the answer in any order.
-
 			Input: nums = [2,7,11,15], target = 9
 			Output: [0,1]
-
 			Input: nums = [3,2,4], target = 6
 			Output: [1,2]
-
-			Input: nums = [3,3], target = 6
-			Output: [0,1]
 		*/
         public int[] TwoSum(int[] nums, int target)
 		{
@@ -51,12 +43,39 @@ namespace CSharp_Exercises.LeetCode.Easy
 			}
 			
 			return indexes;
-		} 
+		}
 
-        
+        /* 9. Palindrome Number
+			Given an integer x, return true if x is a palindrome, and false otherwise.
+			Input: 121, Output: true
+			Input: -121, Output: false
+         */
+		public bool IsPalindrome(int x)
+		{
+			// All negatives aren't palindromes
+			if (x < 0) { return false; }
+
+			// Break int arg into digit array
+			// Is there a more efficient way to do this?
+			char[] xChar = x.ToString().ToCharArray();
+			int[] xArray = new int[xChar.Length];
+			for (int i = 0; i < xChar.Length; i++)
+			{
+                xArray[i] = xChar[i] - '0'; // (int)xChar[i] and Convert.ToInt32(xChar[i]) work too
+                i++;
+            }
 
 
-        /* 20. Valid Parentheses
+            Console.WriteLine();
+
+			// LINQ version of what's above
+            // int[] result = yourInt.ToString().Select(o=> Convert.ToInt32(o) - 48 ).ToArray()
+
+            return true;
+		}
+
+
+        /* 20. Valid Parentheses ***Solved
 			Given a string s containing just the characters
 			'(', ')', '{', '}', '[' and ']', determine if
 			the input string is valid.
@@ -86,22 +105,6 @@ namespace CSharp_Exercises.LeetCode.Easy
 		*/
 		public int[] PlusOne(int[] digits)
 		{
-			// Add each digit to a string
-			/*string numStr = "";
-			for (int i = 0; i < digits.Length ; i++)
-			{
-				numStr += digits[i].ToString();
-			}
-			numStr = (Int64.Parse(numStr) + 1).ToString();
-			// Increase digits' length by 1
-			digits = new int[numStr.Length];
-			for (int i = 0; i < numStr.Length; i++)
-			{
-				var x = Int64.Parse(numStr[i].ToString());
-				digits[i] = (int)Int64.Parse(numStr[i].ToString());
-				Console.WriteLine(digits);
-			}*/
-
 			return digits;
 		}
     }
