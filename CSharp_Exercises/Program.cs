34b85a48cb282b1f8b442e07f0e73afd457cef19
WilliamPrakash <willprakash@gmail.com>
2025-04-04 13:59:00 -0400

    Added in Queue data structure

--- CSharp_Exercises/Program.cs
+++ CSharp_Exercises/Program.cs
@@ -5,5 +5,13 @@ using CSharp_Exercises.LeetCode;
 /* Objects */
 Easy_Problems easyProblems = new Easy_Problems();
 Medium_Problems mediumProblems = new Medium_Problems();
-// pwwkew
-mediumProblems.LengthOfLongestSubstring(" ");
+
+/* Data Structores */
+Queue_Custom<string> q = new Queue_Custom<string>(10);
+q.enqueue("aa");
+q.enqueue("oop");
+q.enqueue("REST");
+q.enqueue("LINQ");
+q.printQueue();
+q.dequeue();
+q.printQueue();
