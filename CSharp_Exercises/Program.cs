dc027c78b27e64552cf059bf5b1694400fddf682
WilliamPrakash <willprakash@gmail.com>
2025-04-20 15:52:57 -0400

    Implemented a bubble sort algorithm

--- CSharp_Exercises/Program.cs
+++ CSharp_Exercises/Program.cs
@@ -1,29 +1,10 @@
 ﻿using CSharp_Exercises.Data_Structures;
 using CSharp_Exercises.LeetCode;
-
+using CSharp_Exercises.Sorting_Algorithms;
 
 /* Objects */
 Easy_Problems easyProblems = new Easy_Problems();
 Medium_Problems mediumProblems = new Medium_Problems();
 
-Deque<int> dairyQueen = new Deque<int>(10);
-dairyQueen.enqueueFront(5);
-dairyQueen.enqueueFront(2);
-dairyQueen.enqueueFront(17);
-dairyQueen.enqueueFront(8);
-dairyQueen.enqueueFront(14);
-dairyQueen.enqueueFront(101);
-dairyQueen.enqueueFront(73);
-// 5 2 17 8 14 101 73 blank blank blank
-dairyQueen.enqueueBack(13);
-dairyQueen.enqueueBack(30);
-dairyQueen.enqueueBack(7);
-dairyQueen.enqueueBack(17947);
-// 5 2 17 8 14 101 73 7 30 13
-dairyQueen.printDeque();
-
-dairyQueen.dequeueFront();
-dairyQueen.dequeueFront();
-dairyQueen.dequeueBack();
-
-dairyQueen.printDeque();
+BubbleSort bubSort = new BubbleSort([9,1,20,58,12,7]);
+bubSort.Sort();
