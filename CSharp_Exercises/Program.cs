cce5d54423781baad6509d9c7538c5c6c4056b72
WilliamPrakash <willprakash@gmail.com>
2025-04-28 20:44:32 -0400

    Delegates

--- CSharp_Exercises/Program.cs
+++ CSharp_Exercises/Program.cs
@@ -1,10 +1,11 @@
-﻿using CSharp_Exercises.Data_Structures;
+﻿using CSharp_Exercises.Concepts;
+using CSharp_Exercises.Data_Structures;
 using CSharp_Exercises.LeetCode;
 using CSharp_Exercises.Sorting_Algorithms;
 
 /* Objects */
 Easy_Problems easyProblems = new Easy_Problems();
 Medium_Problems mediumProblems = new Medium_Problems();
+Concepts cs = new Concepts();
 
-BubbleSort bubSort = new BubbleSort([9,1,20,58,12,7]);
-bubSort.Sort();
+cs.DemonstrateDelegates();
